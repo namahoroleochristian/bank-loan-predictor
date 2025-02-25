1298d479ce54b2f1aaeaaf94c6b0771f907b5135
namahoroleochristian <leonamahoro1@gmail.com>
2025-02-24 19:28:28 -0800

    normalised LoanAmount feature

--- src/bank-loan.py
+++ src/bank-loan.py
@@ -1,10 +1,12 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 
 df = pd.read_csv('Loan-Approval-Prediction.csv')
 # print(df.head(),"head data")    
-# print(df.describe(),"df description")    
+print(df.describe(),"df description")    
 print(df.isnull().sum(),"sum of null values on our data set")    
 categorical_data = ["Gender","Married", "Self_Employed", "Dependents"]
 
@@ -14,8 +16,11 @@ for col in categorical_data:
 df["LoanAmount"].fillna(df["LoanAmount"].mean(),inplace=True)
 df["Credit_History"].fillna(df["Credit_History"].mode()[0],inplace=True)
 df["Loan_Amount_Term"].fillna(df["Loan_Amount_Term"].mode()[0],inplace=True)
-
 df.drop_duplicates(inplace=True)
 df.to_csv("Cleaned-Loan-Approval-Prediction.csv")
+
 df["ApplicantIncome"] = (df["ApplicantIncome"] - df["ApplicantIncome"].mean()) / df["ApplicantIncome"].std() 
-print(df["ApplicantIncome"],"df info")    
+df["LoanAmount"] = (df["LoanAmount"] - df["LoanAmount"].mean() ) / df["LoanAmount"].std()
+
+# df.drop("")
+print(df["LoanAmount"],"df info")    
